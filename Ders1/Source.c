d13916baa71ae9240bb2e04a2f4ffa6d33311ed3
sanoxy <43489338+sanoxy@users.noreply.github.com>
2022-02-27 06:13:54 -0800

    Ders1

---
+++ Ders1/Source.c
@@ -0,0 +1,249 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#pragma warning (disable:4996)
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	//Output
+	
+	/*printf("Hello World!");*/
+	/*int grade = 90;
+	int ID = 500;
+	char classLevel = 'A';
+
+	printf("ID: %d\nClass: %c\tGrade: %d\n\n", ID, classLevel, grade);*/
+
+	/*float number1 = 13.5;
+	double number2 = 12.4;
+
+	printf("Number1: %f\n", number1);
+	printf("Number2: %.2lf\n", number2);*/
+
+	/*char araba = 'a';
+	
+	printf("araba=%c", araba);*/
+
+	//INPUT
+
+	//int testInteger;
+	//printf("Enter an integer: ");
+	//scanf("%d", &testInteger);
+
+	//printf("Number = %d", testInteger);
+
+	/*float num1;
+	double num2;
+
+	printf("Enter number1: ");
+	scanf("%f", &num1);
+	printf("Enter number2; ");
+	scanf("%lf", &num2);
+
+	printf("Number1 = %f\nNumber2 = %lf", num1, num2);*/
+
+	/*int a;
+	float b;
+
+	printf("Enter integer and then a float: ");
+	scanf("%d %f", &a, &b);
+
+	printf("You Entered: %d and %f", a, b);*/
+
+	/*
+					OPERATORS
+
+		1. Arithmetic Operators
+			+
+			-
+			*
+			/
+			%
+
+		2. Assignment Operators
+			=
+			+=
+			-=
+			*=
+			/=
+			%=
+
+			a += 5  ===> a = a+5;
+
+		3. Relational Operators
+			==			Equal to						5 == 3 ===> 0
+			>			Greater Than					5 > 3  ===> 1
+			<			Less Than						5 < 3  ===> 0
+			!=			Not Equal To					5 != 3 ===> 1
+			>=			Greater than or equal to		5 >= 3 ===> 1
+			<=			Less than or equal to			5 <= 3 ===> 0
+
+		4. Logical Operators
+			&&			True only if all operands are true			(c == 5) && (d > 5)
+			||			True only if either one operand is true		(c == 5) || (d > 5)
+			!			Ture only if the operand is 0				!(c==5)
+
+		5. Bitwise Operators
+			&
+			|
+			^
+			~
+			<<
+			>>
+
+		6. Other Operators
+			,
+			sizeof()
+
+
+
+	*/
+
+	//int a = 9;
+	//int b = 4;
+	//int c;
+
+	//c = a + b;	
+	//printf("A+B = %d\n", c);
+	//c = a - b;
+	//printf("A-B = %d\n", c);
+	//c = a * b;
+	//printf("A*B = %d\n", c);
+	//c = (double)a / b;
+	//printf("A/B = %lf\n", c);
+	//c = a % b;
+	//printf("A%cB = %d\n",'%', c);
+
+	/*int a = 5;
+	int c;
+
+	c = a;
+	printf("C = %d\n", c);
+	c += a;
+	printf("C = %d\n", c);
+	c -= a;
+	printf("C = %d\n", c);
+	c *= a;
+	printf("C = %d\n", c);
+	c /= a;
+	printf("C = %d\n", c);
+	c %= a;
+	printf("C = %d\n", c);*/
+
+	//int a = 5;
+	//int b = 5; 
+	//int c = 10;
+	//int result;
+
+	//result = (a == b) && (c > b);
+	//printf("(a == b) && (c > b) is %d \n", result);		//1
+
+	//result = (a == b) && (c < b);
+	//printf("(a == b) && (c < b) is %d \n", result);		//0
+
+	//result = (a == b) || (c < b);
+	//printf("(a == b) || (c < b) is %d \n", result);		//1
+
+	//result = (a != b) || (c < b);
+	//printf("(a != b) || (c < b) is %d \n", result);		//0
+
+	//result = !(a != b);
+	//printf("!(a != b) is %d \n", result);				//1
+
+	//result = !(a == b);
+	//printf("!(a == b) is %d \n", result);				//0
+
+
+	/*int a = 5;
+	int b = 10;
+	int c = 15;
+
+	int a = 5, b = 10, c = 15;*/
+
+	//int a;
+	//float b;
+	//double c;
+	//char d;
+
+	//printf("Size of int =%lu bytes\n", sizeof(a));
+	//printf("Size of float=%lu bytes\n", sizeof(b));
+	//printf("Size of double=%lu bytes\n", sizeof(c));
+	//printf("Size of char=%lu byte\n", sizeof(d));
+
+	/*
+			a = 5;
+			b = 10;
+
+			5
+			10
+
+			a = 10;
+			b = 5;
+	
+			Enter first number: 1.20
+			Enter second number: 2.45
+	
+			After swapping, first number = 2.45
+			After swapping, second number = 1.20
+	
+	*/
+
+	/*int a, b, c;
+	printf("Enter first number: ");
+	scanf("%d", &a);
+
+	printf("Enter second number: ");
+	scanf("%d", &b);
+
+	c = b;
+	b = a;
+	a = c;
+
+	printf("\nAfter swapping, first number = %d\n", a);
+	printf("After swapping, second number = %d", b);*/
+
+	/*
+				ASCII
+	
+		erkan
+		ayberk
+		serhat
+		batu
+		ömer
+
+	
+	*/
+
+	/*printf("%d", 'A');*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
