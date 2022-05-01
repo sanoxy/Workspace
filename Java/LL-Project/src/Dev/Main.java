62409988262bc7d8ef83097a4e9851115e6f9365
sanoxy <43489338+sanoxy@users.noreply.github.com>
2022-05-01 10:21:54 -0700

    Update

---
+++ Java/LL-Project/src/Dev/Main.java
@@ -0,0 +1,10 @@
+package Dev;
+
+import java.util.LinkedList;
+
+public class Main {
+    LinkedList<Student> students = new LinkedList<Student>();
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java/OOP3/src/OOP3/Animal.java
@@ -0,0 +1,15 @@
+package OOP3;
+
+public class Animal {
+    class Reptile{
+        public void displayInfo(){
+            System.out.println("I am a reptile!");
+        }
+    }
+
+    static class Mammal {
+        public void displayInfo(){
+            System.out.println("I am a mammal");
+        }
+    }
+}
