ae2715cdcab7df756c70e8a9017121ea3ff0a73d
Erkan Ozcan (eozcan) <eozcan@micron.com>
2022-07-16 13:12:23 -0700

    Update!

---
+++ Java/HashTable/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Hashtable;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        String x = "Erkan";
+//        System.out.println(x.hashCode());
+//
+//        Hashtable<Integer,String> table = new Hashtable<>();
+//
+//        table.put(1,"Erkan");
+//        table.put(2,"Ayberk");
+//        table.put(3,"Mehmet");
+//
+//        System.out.println(table.containsKey(3));
+//        System.out.println(table.containsValue("Mehmet"));
+//
+//        for(Map.Entry<Integer,String> entry : table.entrySet()){
+//            System.out.println("Key: " + entry.getKey() + "\nValue: " + entry.getValue()+"\n");
+//        }
+//
+//        table.remove(2);
+//        System.out.println("?????????????????????????????????");
+//
+//        for(Map.Entry<Integer,String> entry : table.entrySet()){
+//            System.out.println("Key: " + entry.getKey() + "\nValue: " + entry.getValue()+"\n");
+//        }
+
+    }
+}
